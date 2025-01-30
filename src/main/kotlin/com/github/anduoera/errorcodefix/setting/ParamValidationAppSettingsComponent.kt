6e9619e267f3896623cf9f6a4571c185e98d2c8e
anduoer <anduoera@gmail.com>
2025-01-30 12:14:37 +0800

    feat:完成所有

--- src/main/kotlin/com/github/anduoera/errorcodefix/setting/ParamValidationAppSettingsComponent.kt
+++ src/main/kotlin/com/github/anduoera/errorcodefix/setting/ParamValidationAppSettingsComponent.kt
@@ -1,6 +1,7 @@
 package com.github.anduoera.errorcodefix.setting
 
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBTextArea
 import com.intellij.ui.components.JBTextField
 import com.intellij.util.ui.FormBuilder
 import java.awt.Component
@@ -11,12 +12,32 @@ class ParamValidationAppSettingsComponent() {
     private var myMainPanel = JPanel()
     private val myUriText = JBTextField()
     private val myShowTextTypeComboBox = JComboBox<String>()
+    private val myExampleTextArea = JBTextArea(7, 40)
 
     init {
         myShowTextTypeComboBox.addItem("show remark")
         myShowTextTypeComboBox.addItem("show validation value")
+
+        myExampleTextArea.isEditable = false
+        myExampleTextArea.lineWrap = true // 自动换行
+        myExampleTextArea.wrapStyleWord = true // 使换行更自然
+        myExampleTextArea.isOpaque = false // 透明背景
+        myExampleTextArea.background = null // 继承父级背景色
+        myExampleTextArea.border = null // 移除边框
+        myExampleTextArea.text = """
+           [
+                {
+                    "param_name": "ak_pin",
+                    "validator_pattern": "alias",
+                    "validator_value": "numeric,min=2,max=8",
+                    "remark": "Pin Code\n支持字符类型：数字\n字符长度：2~8"
+                }
+           ]
+        """.trimIndent()
+
         myMainPanel = FormBuilder.createFormBuilder()
             .addLabeledComponent(JBLabel("Uri"), myUriText, 1, false)
+            .addLabeledComponent(JBLabel("Example"), myExampleTextArea, 1, false)
             .addLabeledComponent(JBLabel("Show "), myShowTextTypeComboBox, 1, false)
             .addComponentFillVertically(JPanel(), 0)
             .panel
